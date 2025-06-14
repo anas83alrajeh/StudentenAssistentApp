a0638c678fb96ae7d3975663c1ab0d75ea5c66c8
Anas Al Rajeh <160426061+anas83alrajeh@users.noreply.github.com>
2025-06-14 02:28:48 +0200

    Update similarity_search.py

--- similarity_search.py
+++ similarity_search.py
@@ -3,6 +3,12 @@ from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def find_most_similar_paragraphs(question, paragraphs, top_k=3):
+    if not paragraphs:
+        return "Keine Inhalte gefunden."  # لا توجد فقرات
+
+    if len(paragraphs) < top_k:
+        top_k = len(paragraphs)
+
     question_emb = model.encode(question, convert_to_tensor=True)
     paragraphs_emb = model.encode(paragraphs, convert_to_tensor=True)
     similarities = util.pytorch_cos_sim(question_emb, paragraphs_emb)[0]
