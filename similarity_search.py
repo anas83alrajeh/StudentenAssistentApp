530a5076b6886d16039f8ede199ed8b49dd961d5
Anas Al Rajeh <160426061+anas83alrajeh@users.noreply.github.com>
2025-06-14 02:27:23 +0200

    Update similarity_search.py

--- similarity_search.py
+++ similarity_search.py
@@ -6,7 +6,6 @@ def find_most_similar_paragraphs(question, paragraphs, top_k=3):
     question_emb = model.encode(question, convert_to_tensor=True)
     paragraphs_emb = model.encode(paragraphs, convert_to_tensor=True)
     similarities = util.pytorch_cos_sim(question_emb, paragraphs_emb)[0]
-    top_results = similarities.topk(k=top_k)
+    top_results = similarities.topk(top_k)
     top_paragraphs = [paragraphs[idx] for idx in top_results.indices]
-    # دمج أفضل الفقرات الثلاث
     return "\n\n".join(top_paragraphs)
