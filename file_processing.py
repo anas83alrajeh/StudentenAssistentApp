e7e252fefa15577f752ab79e92460acb84a24502
Anas Al Rajeh <160426061+anas83alrajeh@users.noreply.github.com>
2025-06-14 02:19:08 +0200

    Update file_processing.py

--- file_processing.py
+++ file_processing.py
@@ -1,7 +1,5 @@
 import fitz  # PDF
 from docx import Document  # Word
-from PIL import Image  # Bilder
-import pytesseract  # OCR
 
 def extract_text(file, filetype):
     if filetype == "pdf":
@@ -15,24 +13,5 @@ def extract_text(file, filetype):
         document = Document(file)
         return "\n".join([para.text for para in document.paragraphs])
 
-    elif filetype in ["jpg", "jpeg", "png"]:
-        image = Image.open(file)
-        return pytesseract.image_to_string(image)
-
     else:
-        return "🚫 Typ nicht unterstützt."
-
-def split_text_to_paragraphs(text, min_length=50):
-    raw_paragraphs = text.split('\n')
-    paragraphs = []
-    temp = ""
-    for line in raw_paragraphs:
-        if line.strip() == "":
-            if len(temp) >= min_length:
-                paragraphs.append(temp.strip())
-            temp = ""
-        else:
-            temp += " " + line.strip()
-    if len(temp) >= min_length:
-        paragraphs.append(temp.strip())
-    return paragraphs
+        return "🚫 Typ nicht unterstützt."  # نوع غير مدعوم
